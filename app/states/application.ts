90bd9887202f138f65587b301e951775655f5190
Antonius Ostermann <ostermann.antonius@gmail.com>
2018-01-14 20:25:25 +0100

    adds cancelGenericIntent and helpGenericIntent, overrides helpGenericIntent for MainState

--- app/states/application.ts
+++ app/states/application.ts
@@ -9,4 +9,14 @@ export class ApplicationState extends BaseState {
   constructor(@unmanaged() responseFactory: unifierInterfaces.ResponseFactory, @unmanaged() translateHelper: i18nInterfaces.TranslateHelper) {
     super(responseFactory, translateHelper);
   }
+
+  /** "Cancel!" */
+  cancelGenericIntent() {
+    this.responseFactory.createVoiceResponse().endSessionWith(this.translateHelper.t());
+  }
+
+  /** "Help me!" */
+  helpGenericIntent() {
+    this.responseFactory.createVoiceResponse().prompt(this.translateHelper.t());
+  }
 }
